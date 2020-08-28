8ba6ae0c4dfe47853a2641a8a0e78b3e785e51f3
n3s7or <nrmorales.93@gmail.com>
2020-08-28 16:27:13 -0400

    Improved caching

--- ofertascx/__init__.py
+++ ofertascx/__init__.py
@@ -24,22 +24,34 @@ def get_page() -> str or bool:
     raise 'Something bad happen while fetching the page: %s' % r.reason
 
 
+def get_offers():
+    cache = Cache()
+    html = get_page()
+
+    for offer in ('ventas', 'compras'):
+        cache.store(offer, list(process_table(selector=offer, page=html)))
+
+
 # TODO Avoid code duplication
 def get_ventas():
     cache = Cache()
     ventas = cache.restore('ventas')
+
     if ventas:
         return ventas
-    ventas = list(process_table(selector='ventas', page=get_page()))
-    cache.store('ventas', ventas)
-    return ventas
+    else:
+        get_offers()
+
+    return cache.restore('ventas')
 
 
 def get_compras():
     cache = Cache()
     compras = cache.restore('compras')
+
     if compras:
         return compras
-    compras = list(process_table(selector='compras', page=get_page()))
-    cache.store('compras', compras)
-    return compras
+    else:
+        get_offers()
+
+    return cache.restore('compras')
